ab8af03fae657689245f3e8bf29ee736f26a9b1f
avoajaugochukwu <karlthegoo@hotmail.com>
2021-10-17 10:12:43 +0100

    Colony Initialized completed

--- src/api.ts
+++ src/api.ts
@@ -7,11 +7,15 @@ import {
 import { Wallet, utils } from "ethers";
 import { getColonyClient, getUserAddress } from "./utils";
 
+/* 
+  @Note: I choose to create objects for merging raw logs and parsed logs, 
+  because it provides more clarity when consuming the object.
+
+*/
+
 export const getPayoutClaimed = async () => {
   const colonyClient = await getColonyClient();
-
   const eventFilter = colonyClient.filters.PayoutClaimed(null, null, null);
-
   const eventLogs = await getLogs(colonyClient, eventFilter);
 
   const parsedLogs = eventLogs.map((event) =>
@@ -34,4 +38,24 @@ export const getPayoutClaimed = async () => {
   });
 
   // console.log(combined)
+  getColonyInitialised();
+};
+
+export const getColonyInitialised = async () => {
+  const colonyClient = await getColonyClient();
+  const eventFilter = colonyClient.filters.ColonyInitialised(null, null);
+  const eventLogs = await getLogs(colonyClient, eventFilter);
+
+  const parsedLogs = eventLogs.map((event) =>
+    colonyClient.interface.parseLog(event)
+  );
+
+  console.log(eventLogs);
+  console.log(parsedLogs);
+
+  const combined = eventLogs.map((p, index) => {
+    return { eventLog: p, parsedLog: parsedLogs[index] };
+  });
+
+  console.log(combined)
 };
--- src/utils.ts
+++ src/utils.ts
@@ -51,3 +51,5 @@ export const getUserAddress = async (singleLog, colonyClient) => {
 
   return userAddress;
 };
+
+
